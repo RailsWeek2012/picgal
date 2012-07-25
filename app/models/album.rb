8c327f97369a64f8cdbf98758b818de296ab2dc4
xwrstfgl <robert.palm@mni.thm.de>
2012-07-25 21:11:29 +0200

    Member list: Added number of galleries for each user

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
+  belongs_to :user
   has_many :pictures
   has_many :comments, :as => :commentable, :dependent => :destroy
   attr_accessible :name, :user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :groups
   has_many :comments, :as => :commentable, :dependent => :destroy
   has_many :comments
+  has_many :albums
   attr_accessible :name, :email, :def_licence, :description, :email_notify_comment, :password_digest, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   validates :email, format: /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i #, uniqueness: true
